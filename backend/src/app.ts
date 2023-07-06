99febbdeefcb5bcad3d37f8d1982b8e8bfe8e241
MustafaGuer <mustafaguer@outlook.com>
2023-07-06 22:08:42 +0200

    start backend code & first api routes set & default error handling added

---
+++ backend/src/app.ts
@@ -0,0 +1,21 @@
+import express, { NextFunction, Request, Response } from "express";
+import { json } from "body-parser";
+
+import placesRoutes from "./routes/places-routes";
+import HttpError from "./shared/model/http-error";
+
+const app = express();
+
+app.use(json());
+
+app.use("/api/places", placesRoutes);
+
+app.use((error: HttpError, req: Request, res: Response, next: NextFunction) => {
+  if (res.headersSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
+app.listen(5000);
---
+++ backend/src/routes/places-routes.ts
@@ -0,0 +1,44 @@
+import { Router } from "express";
+
+import HttpError from "../shared/model/http-error";
+
+const router = Router();
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    address: "20 W 34th St., New York, NY 10001, United States",
+    creator: "u1",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/800px-Empire_State_Building_%28aerial_view%29.jpg",
+    location: { lat: 40.7484445, lng: -73.9882393 },
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id!", 404);
+  }
+
+  res.status(200).json({ place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => p.creator === userId);
+
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided user id!", 404)
+    );
+  }
+
+  res.status(200).json({ place });
+});
+
+export default router;
